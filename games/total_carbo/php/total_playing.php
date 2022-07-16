f753366164dff63a43f6a1a45f595b2e54a914b2
Yuki Nagata <marin11142000@icloud.com>
2022-07-16 17:35:37 +0900

    ゲーム画面作成

--- games/total_carbo/php/total_playing.php
+++ games/total_carbo/php/total_playing.php
@@ -1,5 +1,12 @@
 
 <?php
+    session_start();
+    require('../../../common.php');
+
+    if(isset($_SESSION['user_id']) && isset($_SESSION['user_name'])){
+        $user_id = $_SESSION['user_id'];
+        $user_name = $_SESSION['user_name'];
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,8 +15,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../../../common.css">
+    <link rel="stylesheet" href="../css/total_playing.css">
 </head>
 <body>
-    ゲーム
+    <header>
+        <a href="../php/total_carbo.php">ゲームをやめる</a>
+        <?= $user_name ?>さん
+    </header>
+    <main></main>
+    <footer></footer>
 </body>
 </html>
