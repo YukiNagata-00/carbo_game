41548340a5681803c776c7119d166d372c7000b4
Yuki Nagata <marin11142000@icloud.com>
2022-09-21 21:02:51 +0900

    getFood functionへリファクタ

--- games/carbo_ate/php/carbo_array.php
+++ games/carbo_ate/php/carbo_array.php
@@ -7,23 +7,31 @@ require('../../../common.php');
 if(isset($_SESSION['user_name']) && isset($_SESSION['user_name'])){
     $user_id = $_SESSION['user_id'];
     $user_name = $_SESSION['user_name'];
+    $type = $_SESSION['type'];
 }else{
     var_dump("failed");
     header('Location: carbo_ate.php');
     exit();
 }
 
-
 $db = dbconnect();
 
-
-$foods = [];
-$records = $db->query("SELECT name, carbo, image FROM foods ORDER BY RAND() LIMIT 5");
-if($records){
+function getFood($db){
+    $records = $db->query("SELECT name, carbo, image FROM foods ORDER BY RAND() LIMIT 5");
+    if($records){
     while($record = $records->fetch_assoc()){
         $foods[]= $record;
     }
+    return $foods;
 }
+}
+
+echo $type;
+
+$foods = getFood($db);
+
+
+
 print_r($foods);
 
 //foods配列のインデックス
@@ -41,6 +49,6 @@ if(isset($foods)){
     $_SESSION['q_index'] = $q_index;
     $_SESSION['result'] = $result;
     $_SESSION['point'] = $point;
-    header('Location: ate_playing.php');
+    // header('Location: ate_playing.php');
 }
 ?>
