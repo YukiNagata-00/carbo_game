9756673dc3dc11cc36b6519383ae2f26b9c7ac2b
Yuki Nagata <marin11142000@icloud.com>
2022-10-08 20:09:55 +0900

    add items in toppage

--- views/top.php
+++ views/top.php
@@ -17,6 +17,8 @@ require('../controller/top/top.php');
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet">
     <!-- <link rel="stylesheet" href="../common.css"> -->
     <link rel="stylesheet" href="../assets/css/top.css">
     <?php
@@ -31,7 +33,22 @@ require('../controller/top/top.php');
     <header role="banner"></header>
 
     <div class="main">
+        <div class="title">MENU</div>
+        <div class="items">
+            <a href=""  class="item">
+                <img src="" alt="">
+                <p>QUIZ</p>
+            </a>
+            <a href="" class="item">
+                <img src="" alt="">
+                <p>GAME</p>
+            </a>
+            <a href="" class="item">
+                <img src="" alt="">
+                <p>FLASH CARD</p>
+            </a>
 
+        </div>
     </div>
     <script type="text/javascript" src = "../parts/_drawer.js"></script>
 </body>
