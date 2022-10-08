f62c0cef3ccb4f8643a2134defd0d0840bde66c9
Yuki Nagata <marin11142000@icloud.com>
2022-10-08 16:59:52 +0900

    add header drawer

--- views/top.php
+++ views/top.php
@@ -17,11 +17,22 @@ require('../controller/top/top.php');
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="../common.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <!-- <link rel="stylesheet" href="../common.css"> -->
+    <link rel="stylesheet" href="../assets/css/top.css">
+    <?php
+    include('../parts/_drawer_links.php');
+    ?>
     <title>Document</title>
 </head>
-<body>
-    
+<body class="drawer drawer--left">
+    <?php
+    include('../parts/_header.php');
+    ?>
+    <header role="banner"></header>
+
+    <div class="main">
+
+    </div>
+    <script type="text/javascript" src = "../parts/_drawer.js"></script>
 </body>
 </html>
