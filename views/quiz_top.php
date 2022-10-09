11d90793c9e784361cd70785b6df4cd3e070c795
Yuki Nagata <marin11142000@icloud.com>
2022-10-09 17:27:19 +0900

    add drawer

--- views/quiz_top.php
+++ views/quiz_top.php
@@ -9,8 +9,20 @@ require('../controller/quiz/index.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/quiz_top.css">
+    <?php
+    include('../parts/_drawer_links.php');
+    ?>
 </head>
-<body>
+<body class="drawer drawer--left">
+    <?php 
+        include('../parts/_header.php');
+    ?>
+    <header role="banner"></header>
     
+    <div class="main"></div>
+    <script type="text/javascript" src = "../parts/_drawer.js"></script>
 </body>
 </html>
