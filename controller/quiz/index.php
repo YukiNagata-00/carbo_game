d661dc150f33a20bacd5ecf5c4855133f028c127
Yuki Nagata <marin11142000@icloud.com>
2022-10-11 12:58:51 +0900

    選択肢　実装

--- controller/quiz/index.php
+++ controller/quiz/index.php
@@ -4,9 +4,15 @@ require_once('../config.php');
 $db = dbconnect();
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     if(isset($_POST['easy_start_btn'])){
+    
         $quiz = new EasyQuiz($db);
-        var_dump($quiz->foods[0]['carbo']);
-        var_dump($quiz->choices[0]);
+        // if(isset($SESSION['choices'])){
+        //     unset($SESSION['choices']);
+        // }
+        var_dump($quiz->foods);
+        var_dump($quiz->choices);
+        // $_SESSION['foods'] =  $quiz->foods;
+        // $_SESSION['choices'] =  $quiz->choices;
         header('Location: quiz_playing.php');
         exit();
     }
--- controller/quiz/quiz_playing.php
+++ controller/quiz/quiz_playing.php
@@ -1,5 +1,24 @@
 <?php
 $db = dbconnect();
+// var_dump($_SESSION['foods']);
+$foods = $_SESSION['foods'];
+$index =  $_SESSION['current_num'];
+$point =  $_SESSION['point'];
+$result =  $_SESSION['result'];
+var_dump( $point);
+var_dump($result);
+$choices = $_SESSION['choices'];
+// var_dump($choices);
+
+if(isset($_GET['selected_choice'])){
+    echo $_GET['selected_choice'];
+    if($foods[$index]['carbo'] ==  $_GET['selected_choice']){
+        echo "reight";
+        $point += 20;
+    }else{
+        echo "wrong";
+    }
+}
 
 
 ?>
